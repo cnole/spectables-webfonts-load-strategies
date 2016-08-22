1cbbed420e0fb67c5d03343848e85f71cc39db2f
Zheng Luo <cnoleherzer@gmail.com>
2016-08-22 22:07:21 +0200

    Added some slides.

--- presentation/index.js
+++ presentation/index.js
@@ -21,6 +21,7 @@ import {
   Spectacle,
   Text
 } from "spectacle";
+import CodeSlide from 'spectacle-code-slide';
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
@@ -34,9 +35,14 @@ import Interactive from "../assets/interactive";
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
+require("../assets/theme.css");
 
 
 const images = {
+  darkBackground: require('../assets/dark-background.jpg'),
+  fontLoad: require('../assets/font-load.png'),
+  foit: require('../assets/foit.png'),
+  fout: require('../assets/fout.png'),
   city: require("../assets/city.jpg"),
   kat: require("../assets/kat.png"),
   logo: require("../assets/formidable-logo.svg"),
@@ -46,30 +52,190 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#F6D528",
+  secondary: "#ff4081",
+  tertiary: "#3790A2"
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
+        <Deck transition={[]} transitionDuration={500}>
+          <Slide transition={[]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+              Webfont
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit textColor="primary">
+              60%
+            </Heading>
+            <Heading size={1} fit caps textColor="white">
+              Der Webseiten verwenden Webfonts
+            </Heading>
+            <Heading size={10} textColor="grey" marginTop={20}>
+              60% von den Top 1000 Websites. 2010 waren es noch 0%
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="secondary">
+              3 Wenfonts
+            </Heading>
+            <Heading size={1} fit caps textColor="white">
+              werden durchschnittlich pro Seite geladen
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="tertiary">
+              200KB Bis 400KB
+            </Heading>
+            <Heading size={1} fit caps textColor="white">
+              An Webfonts werden durchschnittlich runtergeladen
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="white">
+              Webfonts sind
+            </Heading>
+            <Heading size={1} fit caps textColor="secondary">
+              Renderblocking
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Image src={images.fontLoad.replace("/", "")} width="100%" />
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Image src={images.foit.replace("/", "")} width="100%" />
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Image src={images.fout.replace("/", "")} width="100%" />
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="tertiary">
+              FOUT ist Progressive enhancement
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="secondary">
+              Webfonts
+            </Heading>
+            <Heading size={1} fit caps textColor="primary">
+              load strategies
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="primary">
+              verwenden keine Webfonts
+            </Heading>
+          </Slide>
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit textColor="primary">
+              @font-face
+            </Heading>
+            <Appear fid="1">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                And hope for the best ;)
+              </Heading>
+            </Appear>
+          </Slide>
+          <CodeSlide transition={[]} lang="html" code={require('raw!../assets/font-face.html')}
+                     ranges={[
+                       { loc: [7, 14] }
+                     ]} />
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit textColor="primary">
+              font-display
+            </Heading>
+            <Appear fid="1">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Noch keine Unterstützung
+              </Heading>
+            </Appear>
+          </Slide>
+          <CodeSlide transition={[]} lang="xml" code={require('raw!../assets/font-display.html')}
+                     ranges={[
+                       { loc: [7, 15] },
+                       { loc: [13, 14], note: "non-blocking mit Fallback und swap onload" }
+                     ]} />
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="primary">
+              Preload
+            </Heading>
+            <Appear fid="1">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Limitierte Browser-Unterstützung
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Blockiert initial rendering
+              </Heading>
+            </Appear>
+            <Appear fid="3">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Unmöglich, repaints/reflows zu gruppieren
+              </Heading>
+            </Appear>
+          </Slide>
+          <CodeSlide transition={[]} lang="html" code={require('raw!../assets/font-preload.html')}
+                     ranges={[
+                       { loc: [8, 15] },
+                       { loc: [6, 7], note: "<link> mit Preload" }
+                     ]} />
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="primary">
+              Inline Data URI
+            </Heading>
+            <Appear fid="1">
+              <Heading size={4} textColor="tertiary" margin="50px auto">
+                Kein FOIT und FOUT
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Blockiert initial rendering
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Skaliert nicht
+              </Heading>
+            </Appear>
+          </Slide>
+          <CodeSlide transition={[]} lang="html" code={require('raw!../assets/font-inline.html')}
+                     ranges={[
+                       { loc: [9, 11], note: "Data URI" }
+                     ]} />
+          <Slide transition={[]} bgImage={images.darkBackground.replace("/", "")}>
+            <Heading size={1} fit caps textColor="primary">
+              Asynchronous
+            </Heading>
+            <Heading size={1} fit caps textColor="primary">
+              Inline Data URI
+            </Heading>
+            <Appear fid="1">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                kurzes FOIT
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Skaliert nicht besser
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={4} textColor="secondary" margin="50px auto">
+                Javascript muss unterstützte Formate kennen
+              </Heading>
+            </Appear>
+          </Slide>
+          <CodeSlide transition={[]} lang="html" code={require('raw!../assets/font-async-inline.html')}
+                     ranges={[
+                       { loc: [16, 17], note: "CSS mit inlined Font per JS nachladen" }
+                     ]} />
+
+          <Slide transition={[]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
             <Heading size={2} caps fit textColor="primary" textFont="primary">
               Wait what?
